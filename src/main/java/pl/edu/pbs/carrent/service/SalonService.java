e629c0fb7a4cf9e78f629f004b6fd6c84c43e227
Krzysztof-Tomaszewski <krzysztof.tom@outlook.com>
2022-06-06 00:39:57 +0200

    Added endpoints for operations on salons

--- src/main/java/pl/edu/pbs/carrent/service/SalonService.java
+++ src/main/java/pl/edu/pbs/carrent/service/SalonService.java
@@ -1,10 +1,20 @@
 package pl.edu.pbs.carrent.service;
 
+import org.springframework.http.ResponseEntity;
 import pl.edu.pbs.carrent.model.Salon;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SalonService {
 
     Optional<Salon> getSalonById(Long id);
+
+    List<Salon> getSalons();
+
+    Optional<Salon> updateSalon(Long id, Salon salon);
+
+    Optional<Salon> addNewSalon(Salon salon);
+
+    void deleteSalon(Long id);
 }
