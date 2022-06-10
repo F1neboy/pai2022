a042c23c07042426071d7e212bc891b95d2e0bc8
Argondt <58610835+Argondt@users.noreply.github.com>
2022-06-10 18:05:30 +0200

    Revert "Added Mailservice and Contact Controller"
    This reverts commit e499507ef7026e45d06f949acda5af5c4ce32a6b.

--- src/main/java/pl/edu/pbs/carrent/controller/ContactController.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pbs.carrent.controller;
-
-import org.springframework.http.ResponseEntity;
-import pl.edu.pbs.carrent.model.Contact;
-
-public interface ContactController {
-
-    void sendMessage(Contact contact);
-}
--- src/main/java/pl/edu/pbs/carrent/service/MailService.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.pbs.carrent.service;
-
-public interface MailService {
-    void sendMessage(String from, String name, String subject, String content);
-}
