e83deab9d8ff1c0c55a0d1938c49c6cf06e48bbb
beert777 <97989020+beert777@users.noreply.github.com>
2022-06-21 23:53:28 +0200

    Update CarRentApplicationTests.java

--- src/test/java/pl/edu/pbs/carrent/CarRentApplicationTests.java
+++ src/test/java/pl/edu/pbs/carrent/CarRentApplicationTests.java
@@ -5,9 +5,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarRentApplicationTests {
+    private final CarRepository carRepository;
+
+    
 
     @Test
     void contextLoads() {
+        Car car = new Car("Modus","2012 - brazowy","Renault", null, s1, true);
+        carRepository.save(car);
     }
+
 
 }
